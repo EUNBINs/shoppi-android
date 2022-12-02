abb26472001c87b075ba6ba0d7715d95285f8723
jangeunbin <dmsqls891@naver.com>
2022-12-02 18:05:07 +0900

    1202commit
    fragment화면 layout까지 진행함

--- app/src/main/java/com/shoppi/android/MainActivity.kt
+++ app/src/main/java/com/shoppi/android/MainActivity.kt
@@ -2,10 +2,29 @@ package com.shoppi.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    // 공통적으로 어떠한 이벤트를 알려주는 메소드일 경우에는 Android SDK는 on + 메소드 이름으로 되어있다
+    override fun onRestart() {
+        super.onRestart()
+        Log.e(TAG, "onReStart")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e(TAG, "onResume")
+    }
+
 }
