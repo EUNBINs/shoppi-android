3290a69ae53b72fb38622e4d1684efc44878f46c
jangeunbin <dmsqls891@naver.com>
2022-12-05 18:05:34 +0900

    12월5일 commit
    splash 빈화면 에러

--- app/src/main/java/com/shoppi/android/SplashActivity.kt
+++ app/src/main/java/com/shoppi/android/SplashActivity.kt
@@ -3,11 +3,22 @@ package com.shoppi.android
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 
 class SplashActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this, MainActivity::class.java))
-        finish()
+        Log.e("splashActivity", "splash 등장")
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, SPLASH_VIEW_TIME)
+    }
+
+    companion object{
+        val SPLASH_VIEW_TIME : Long = 1000  //  스플래시 화면 지속시간
     }
 }
